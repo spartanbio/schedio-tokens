6429bd7c4eaaa61ef75bf84dc300fb55c7f7bd45
Alex Collier <alexkcollier@gmail.com>
2020-05-07 10:53:00 -0400

    fix(easing)!: avoids `in` keyword

--- dist/web/common-js/easing.common.js
+++ dist/web/common-js/easing.common.js
@@ -1,7 +1,7 @@
 module.exports = {
   easingBase: 'cubic-bezier(0.64, 0, 0.35, 1)',
-  easingIn: 'cubic-bezier(0.36, 0, 1, 1)',
-  easingOut: 'cubic-bezier(0, 0, 0.42, 1)',
+  easingEaseIn: 'cubic-bezier(0.36, 0, 1, 1)',
+  easingEaseOut: 'cubic-bezier(0, 0, 0.42, 1)',
   easingExcite: 'cubic-bezier(0.18, 0.67, 0.6, 1.22)',
   easingOvershoot: 'cubic-bezier(0.07, 0.28, 0.32, 1.22)',
   easingAnticipate: 'cubic-bezier(0.38, -0.4, 0.88, 0.65)',
